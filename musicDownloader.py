026e6bfec177965fd590eabaa98b6e932c0764b9
alex0blackwell <alex.blackwell314@gmail.com>
2019-10-31 23:01:40 -0700

    testing UI in the text doc

--- musicDownloader.py
+++ musicDownloader.py
@@ -1,8 +1,8 @@
-# from __future__ import unicode_literals
 import youtube_dl
 
 
 class MyLogger(object):
+    # For debugging and error throwing
     def debug(self, msg):
         pass
 
@@ -14,7 +14,7 @@ class MyLogger(object):
 
 
 def download(title):
-
+    # Downloads a m4a audio file with the provided title
     outtmpl = '~/Music/' + title + '.%(ext)s'
     ydl_opts = {
         'format': 'm4a',
@@ -23,15 +23,26 @@ def download(title):
     }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        # Get the top result off youtube
         ydl.extract_info("ytsearch:"+title+"lyrics", download=True)
 
 
 def main():
-
+    # File minipulation
     file = open('songList.txt', 'r')
     counter = len(file.readlines())
     file.close()
-    for i in range(counter):
+
+    if(counter != 1):
+        file = open('songList.txt', 'w')
+        file.write("Enter the names of songs below you wish to download:")
+        file.close()
+        file = open('songList.txt', 'r+')
+        file.truncate(0)
+        file.close()
+    print("The file \"songList.txt\" had no content. The fle has now been intialized")
+
+    for i in range(1, counter):
         file = open('songList.txt', 'r')
         line = str(file.readlines()[i])[:-1]
         file.close()
@@ -41,8 +52,14 @@ def main():
         print("Download complete.")
     print(f"Finished all downloads ({i})")
 
+    # Erases contents of file so no need to re-download songs
     file = open('songList.txt', 'r+')
     file.truncate(0)
+    file.close()
+
+    # file = open('songList.txt', 'w')
+    # file.write("Enter the names of songs below you wish to download:")
+    # file.close()
 
 
 main()
