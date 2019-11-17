4ca7007c524b04e935228e37e2c93362d32ea115
alex0blackwell <alex.blackwell314@gmail.com>
2019-11-17 08:44:04 -0800

    Irregular verb file ready to be worked with

--- french-verb-conj/main.py
+++ french-verb-conj/main.py
@@ -1,25 +1,45 @@
-def checkRegular(usrVerb):
-    pass  # later
+# def checkRegular(usrVerb):
+#     if(usrVerb in)
 
 
-def clean(origVerb):
-    cleanVerb = origVerb
-    placement = 0
-    for char in origVerb:
-        if(char == 'é' or char == 'ê'):
-            origVerb[placement] = 'e'
-        if(char == 'ï' or char == 'î'):
-            origVerb[placement] = 'i'
-        placement += 1
-    return cleanVerb
+def clean(origVerbs):
+    cleanVerbs = []
+    for i in range(len(origVerbs)):
+        verb = origVerbs[i]
+        cleanVerb = verb.replace("é" or "ê", "e")
+        cleanVerb = cleanVerb.replace(" ", "")
+        irregFile = open("irregularVerbs.txt", "a")
+        irregFile.write(f"{cleanVerb}\n")
+        irregFile.close()
+        cleanVerbs += [cleanVerb]
+        # placement = 0
+        # currentVerb = origVerbs[i]
+        # for char in origVerbs[i]:
+        #     if(char == 'é' or char == 'ê'):
+        #         currentVerb[placement] = 'e'
+        #     if(char == 'ï' or char == 'î'):
+        #         currentVerb[placement] = 'i'
+        #     if(char == ' '):
+        #         currentVerb[placement] = ''
+        #     placement += 1
+        # cleanVerbs += [currentVerb]
+    return cleanVerbs
 
 
 def main():
-    usrVerb = input("Enter the French verb you wish to conjugate:\n")
-    usrPronoun = input("Enter the pronoun you wish to conjugate for:\n")
-    cleanVerb = clean(usrVerb)
-    checkRegular()
+    irregFile = open("irregularVerbs.txt", "r")
+    irregVerbs = irregFile.read().splitlines()
+    irregFile.close()
 
+    cleanIrregs = clean(irregVerbs)
+
+    print(cleanIrregs)
+
+    # usrVerb = input("Enter the French verb you wish to conjugate:\n")
+    # usrPronoun = input("Enter the pronoun you wish to conjugate for:\n")
+    # cleanVerb = clean(usrVerb)
+    # print(f"You imputted")
+    # checkRegular(usrVerb)
 
 
 main()
