f2d49b4708dde82de05d83933c2345e1e355452a
alex0blackwell <alex.blackwell314@gmail.com>
2019-11-19 14:15:36 -0800

    Vertical lines complete grid

--- gridVisual.py
+++ gridVisual.py
@@ -6,20 +6,32 @@ from PIL import Image, ImageDraw
 def horLines(maxWidth, maxHeight, draw):
     xStart = 0
     xFin = maxWidth
-    y = 0
 
+    y = 0
     while(y < maxHeight):
         horLine = ((xStart, y), (xFin, y))
         draw.line(horLine, fill=128)
         y += 10
 
 
+def vertLines(maxWidth, maxHeight, draw):
+    yStart = 0
+    Yfin = maxHeight
+
+    x = 0
+    while(x < maxWidth):
+        vertLine = ((x, yStart), (x, Yfin))
+        draw.line(vertLine, fill=128)
+        x += 10
+
+
 def main():
-    height = 300
-    width = 300
+    height = 500
+    width = 500
     image = Image.new(mode='L', size=(height, width), color=255)
     draw = ImageDraw.Draw(image)
     horLines(width, height, draw)
+    vertLines(width, height, draw)
     image.show()
 
 
