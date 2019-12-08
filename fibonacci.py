df244dea49be64b975c2d7e87c2a5532e4f85417
Alex Blackwell <31634087+Alex0Blackwell@users.noreply.github.com>
2019-12-07 19:49:36 -0800

    Added comments

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,13 @@
+# Program that, given the length of how many characters of the 
+# fibonacci sequence is to be displayed, will display the fibonacci
+# sequence.
+
+
 def fibonacci(length):
-    res = [1, 1]
+    res = [1, 1]  # Base
     for i in range(2, length):
-        res += [res[i-2] + res[i-1]]
+        res += [res[i-2] + res[i-1]]  # Recursive step
     return res
 
 
-print(fibonacci(10))
+print(fibonacci(10))  # Length of 10
