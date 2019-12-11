dd29bfb54e925a20287d2870e0aae5f83eba4271
Alex Blackwell <31634087+Alex0Blackwell@users.noreply.github.com>
2019-12-10 18:27:50 -0800

    Added comments

--- baseConverter.py
+++ baseConverter.py
@@ -1,14 +1,19 @@
 # Base conversion algorithm
+# Fastest way to convert bases
+# Note this does it in reverse so the strings and the strings elements
+# must be reversed
 
 
 def baseConvert(num, base):
+    # Given a number and a base to convert to
     res = ''
     q = num
     while(q > 0):
         r = q % base
+        # Reverse string because the whole string will eventually be reversed
         res += ',' + str(r)[::-1]
         q = q // base
-    return res[::-1]
+    return res[::-1]  # Return the whole string reversed
 
 
 usrNum = int(input("Enter a number to convert:\n"))
