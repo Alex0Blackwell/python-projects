dab0d79d8f6c2458289642761fdfa9a4afc48650
alex0blackwell <alex.blackwell314@gmail.com>
2019-12-12 00:26:16 -0800

    Added some responses

--- speech-recognition/voiceRecognition.py
+++ speech-recognition/voiceRecognition.py
@@ -1,10 +1,34 @@
 # Voice recognition project
+# This is the voice assistant Grimmels
+# Extremely powerfull
 
 import time as t
+import random as r
 import sounddevice as sd
 import speech_recognition as sr
 from scipy.io.wavfile import write
 import wavio  # Like wario but not as WAAAA
+
+
+def respond(usrInput):
+    unknown = ["My creator doesn't know what he's doing. That's probably you isn't it?",
+               "My name is GRIMMELS (I don't understand)",
+               "I'm a computer program. I don't know how to respond and I have no fear."]
+    greeting = ['hi', 'hello', 'hey grimmels', 'how\'s life', 'how are things',
+                'what\'s cracking', 'what\'s good', 'how are you', 'what\'s up',
+                'what is up', 'how are ya']
+    greetingRes = ["Hello. They call me Grimmels.", "Oh hello!", "Oh Hi!"
+                   "Hello, my name is Grimmels but you can call me \"The Grim Reaper\"",
+                   "Hello! I love meeting new peole! I don't remember any of them..."]
+
+    res = r.choice(unknown)
+    for i in range(len(greeting)):
+        if(greeting[i] in usrInput.lower()):
+            res = r.choice(greetingRes)
+            break
+    if('grimmels' in usrInput.lower()):
+        res += " I Can't believe you know my name! I must be famous!"
+    return res
 
 
 def main():
@@ -27,6 +51,7 @@ def main():
     # Don't accept recording longer than 15 seconds
     myrecording = sd.rec(int(15 * fs))
 
+    # NOTE: I think it might be better to just do the countdown
     input("Press enter to start recording: ")
     offsetTime = t.time() - initialTime
     input("Press enter to stop recording: ")
@@ -52,9 +77,9 @@ def main():
             # noise adjustment. Note this 1 second will always exist because
             # the countdown
             audio = r.record(source, offset=offsetTime-1, duration=durationTime)
-        print('entered bottom else', durationTime)
         words = r.recognize_google(audio)
-        print(words)
+        print("\t\t", words)
+        print(respond(words))
 
 
 main()
