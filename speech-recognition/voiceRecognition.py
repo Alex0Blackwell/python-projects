85367bd6925bf7f4aa78e61c99f18c12e5b5fda8
alex0blackwell <alex.blackwell314@gmail.com>
2019-12-11 16:23:48 -0800

    Destroyed recognision, can save audio from mic

--- speech-recognition/voiceRecognition.py
+++ speech-recognition/voiceRecognition.py
@@ -1,14 +1,22 @@
 # Voice recognition project
 
-import speech_recognition as sr
-
-r = sr.Recognizer()  # Make on instance of Recognizer
-harvard = sr.AudioFile('harvard.wav')
-
-with harvard as source:
-    # Get the data from the file (not parsing any words yet)
-    audio = r.record(source)
+import sounddevice as sd
+from scipy.io.wavfile import write
+# import speech_recognition as sr
 
 
-words = r.recognize_google(audio)
-print(words)
+audioArray = []
+fs = 44100  # Because of the sample rate
+# r = sr.Recognizer()  # Make on instance of Recognizer
+# harvard = sr.AudioFile('harvard.wav')
+
+# with harvard as source:
+#     # Get the data from the file (not parsing any words yet)
+#     audio = r.record(source)
+
+myrecording = sd.rec(int(5 * fs), samplerate=fs, channels=2)
+sd.wait()  # Wait until the recording is finished
+# print(myrecording)
+write('input.wav', fs, myrecording)
+# words = r.recognize_google(audio)
+# print(words)
