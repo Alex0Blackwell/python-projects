f1f496a0093267824ea5b3d7cab8be8b9ba87986
alex0blackwell <alex.blackwell314@gmail.com>
2019-12-14 00:39:32 -0800

    Countdown starts recording, proper grammar displaying timer prints

--- speech-recognition/voiceRecognition.py
+++ speech-recognition/voiceRecognition.py
@@ -54,19 +54,20 @@ def respond(usrInput):
         min = 0
         sec = 0
         if('-' in usrL):
+            # Sometimes it will be in the format of "22-minutes" ect...
             usrL = usrL.replace('-', ' ')
         usrLst = usrL.split()
 
         if('hour' in usrL):
-            numIndex = usrLst.index('hour') - 1
-            if(usrLst[numIndex] == 'one'):
+            numIndex = usrLst.index('hour') - 1  # Get number before "hour"
+            if(usrLst[numIndex] == 'one'):  # 'one' is the only case like this
+                # All other numbers show numerical form eg) 22
                 hour = 1
             else:
                 hour = int(usrLst[numIndex])
         if('minute' in usrL):
-            numIndex = usrLst.index('minute') - 1  # get the number before minute
-            if(usrLst[numIndex] == 'one'):  # 'one' is the only case like this
-                # All other numbers show numerical form eg) 22
+            numIndex = usrLst.index('minute') - 1
+            if(usrLst[numIndex] == 'one'):
                 min = 1
             else:
                 min = int(usrLst[numIndex])
@@ -76,7 +77,17 @@ def respond(usrInput):
                 sec = 1
             else:
                 sec = int(usrLst[numIndex])
-        print(f"Setting a {hour} hour {min} minute {sec} second timer")
+
+        hText = ''
+        mText = ''
+        sText = ''
+        if(hour > 0):
+            hText = str(hour) + ' hour'
+        if(min > 0):
+            mText = str(min) + ' minute'
+        if(sec > 0):
+            sText = str(sec) + ' second'
+        print(f"Ok, setting a {hText} {mText} {sText} timer")
 
         timer(hour, min, sec)
     elif('grimmels' in usrL):
@@ -95,6 +106,8 @@ def main():
     sd.default.samplerate = fs
     sd.default.channels = 2
 
+    initialTime = t.time()
+
     res = ''
     for a in range(3, 0, -1):
         # Countdown (total time: 1.5 secs)
@@ -103,21 +116,15 @@ def main():
         t.sleep(0.5)
     print()
 
-    initialTime = t.time()
     # Don't accept recording longer than 15 seconds
     myrecording = sd.rec(int(15 * fs))
 
-    # NOTE: I think it might be better to just do the countdown
-    input("Press enter to start recording: ")
-    offsetTime = t.time() - initialTime
     input("Press enter to stop recording: ")
     finalTime = t.time() - initialTime
 
-    durationTime = finalTime - offsetTime
-
-    if(durationTime > 15):
+    if(finalTime > 15):
         print("That was way too long and I don't understand much. Even if",
-              "it's short. But jeez that was actually like", durationTime//1,
+              "it's short. But jeez that was actually like", finalTime//1,
               "seconds. JEEZ!")
     else:
         # Save a file of what the mic captured
@@ -132,7 +139,7 @@ def main():
             # In the offset time, subtract 1 because it is used for ambient
             # noise adjustment. Note this 1 second will always exist because
             # the countdown
-            audio = r.record(source, offset=offsetTime-1, duration=durationTime)
+            audio = r.record(source, duration=finalTime)
         words = r.recognize_google(audio)
         print("\t\t", words)
         print(respond(words))
