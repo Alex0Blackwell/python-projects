6d2a80020d8e849d74adae9ffa04f781a0712cca
Alex Blackwell <31634087+Alex0Blackwell@users.noreply.github.com>
2019-12-26 22:21:00 -0800

    Added comments

--- averageChild.py
+++ averageChild.py
@@ -1,7 +1,9 @@
 # Average children in family calculator
+# Made for a curious friend
 
 
 def getAvg(lst):
+    # Sum up all the elements in a list and then divide by how many are in the list
     res = 0
     for el in lst:
         res += el
@@ -10,16 +12,16 @@ def getAvg(lst):
 
 
 def main():
-    childLst = []
+    childLst = []  # List with the number of how many children in a family
     usrIn = ''
     c = 0
-    while(usrIn != 'e'):
+    while(usrIn != 'e'):  # Type 'e' to end
         usrIn = input(f"how many people are in family {c+1}? (e to end):\n")
         if(usrIn.isdigit()):
             childLst += [int(usrIn)]
-        c += 1
-    print("calculating\nThe average children per family is:")
-    print(getAvg(childLst))
+        c += 1  # To keep track of how many families have been entered
+    print("calculating...\nThe average children per family is:")
+    print(getAvg(childLst))  # Display average number of children
 
 
 main()
